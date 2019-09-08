d0b55c81baa4a856e14104acb3460ecb1a41e2c5
Kevin Karma <kevinkarma55@gmail.com>
2019-09-08 02:44:43 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -3,7 +3,8 @@ class CreateShows < ActiveRecord::Migration[5.2]
     create_table :shows do |t|
       t.string :name 
       t.string :network
-      t.day 
+      t.string :day 
+      t.integer :rating 
     end
   end
 end
