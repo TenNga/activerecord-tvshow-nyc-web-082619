9603a269f4602df1a88e934fa8843c2aee5341db
Kevin Karma <kevinkarma55@gmail.com>
2019-09-08 03:06:10 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,11 +1,10 @@
 class Show < ActiveRecord::Base 
   
   def self.highest_rating
-    show = Show.new 
-    show.all.maximum(:rating)
+    self.all.maximum(:rating)
   end
   
   def self.most_popular_show
-    Show.all.where(":rating = ?", self.highest_rating)
+    self.all.where(":rating = ?", self.highest_rating)
   end
 end
